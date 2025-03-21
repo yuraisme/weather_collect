5336b2df3dfee2c442608c3f6b8b1a56b4c4d8e2
yuraisme <yuraisme@mail.ru>
2025-03-21 13:53:09 +0300

    minor changes

--- HomeCenter/HomeCenter/settings.py
+++ HomeCenter/HomeCenter/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-(i0rg!cbl^^r%yalx%zzrdtw5&uan-2%j)ukuc6oyylh3s8xss
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['gitlab-projects.online', 'localhost', '127.0.0.1']
 
 
 # Application definition
